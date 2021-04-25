63dc82f038388f3d81e98a2ebceb03cc9e8b51a8
Johnny-Linn <ps102646@gmail.com>
2021-04-25 22:07:17 +0800

    update

---
+++ src/core/Menu.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth';
+
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: '#ff9900' }
+    } else {
+        return { color: '#ffffff' }
+    }
+}
+
+const Menu = ({ history }) => (
+    <div>
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link className="nav-link" style={isActive(history, "/")} to="/">
+                    Home
+                </Link>
+            </li>
+            {!isAuthenticated() && (
+                <>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, "/Signin")} to="/Signin">
+                            Signin
+                </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, "/Signup")} to="/Signup">
+                            Signup
+                </Link>
+                    </li>
+                </>
+            )}
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span className="nav-link" style={{ cursor: 'pointer', color: '#ffffff' }} onClick={() => signout(() => {
+                        history.push('/')
+                    })}>
+                        Signout
+                </span>
+                </li>
+            )}
+        </ul>
+    </div>
+)
+
+export default withRouter(Menu);
