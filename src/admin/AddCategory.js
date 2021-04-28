1933ab69680dff1dbd7b095469033a917c50b6d1
Johnny-Lin <ps102646@gmail.com>
2021-04-28 23:51:30 +0800

    category create success and error

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { createCategory } from './apiAdmin';
 
 const AddCategory = () => {
     const [name, setName] = useState('')
@@ -21,7 +22,15 @@ const AddCategory = () => {
         setError('')
         setSuccess(false)
         // make request to api to create category
-
+        createCategory(user._id, token, { name })
+            .then(data => {
+                if (data.error) {
+                    setError(data.error)
+                } else {
+                    setError('')
+                    setSuccess(true)
+                }
+            })
     }
 
     const newCategoryForm = () => (
@@ -33,10 +42,36 @@ const AddCategory = () => {
             <button className="btn btn-outline-primary">Create Category</button>
         </form>
     )
+
+    const showSuccess = () => {
+        if (success) {
+            return <h3 className="text-success">{name} is created</h3>
+        }
+    }
+
+    const showError = () => {
+        if (error) {
+            return <h3 className="text-danger">Category is should be unique</h3>
+        }
+    }
+
+    const goBack = () => (
+        <div className="mt-5">
+            <Link to="/admin/dashboard" className="text-warning">
+                Back to Dashboard
+            </Link>
+        </div>
+    )
+
     return (
         <Layout title="Add a new category" description={`Hello ${name}, ready to add a new category`}>
             <div className="row">
-                <div className="col-md-8 offset-md-2"> {newCategoryForm()} </div>
+                <div className="col-md-8 offset-md-2">
+                    {showSuccess()}
+                    {showError()}
+                    {newCategoryForm()}
+                    {goBack()}
+                </div>
             </div>
         </Layout>
     )
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,14 +1,15 @@
 import { API } from "../config";
 
-export const createCategory = (user) => {
+export const createCategory = (userId, token, category) => {
     // console.log(name, email, password)
-    return fetch(`${API}/signup`, {
+    return fetch(`${API}/category/create/${userId}`, {
         method: "POST",
         headers: {
             Accept: 'application/json',
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
         },
-        body: JSON.stringify(user)
+        body: JSON.stringify(category)
     })
         .then(response => {
             return response.json()
