72c908808db05fbdefbacfd0f8980510e05e5970
Johnny-Lin <ps102646@gmail.com>
2021-05-13 13:36:11 +0800

    purchase history

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { Link } from 'react-router-dom'
+import { getPurchaseHistory } from './apiUser';
 
 const Dashboard = () => {
+    const [history, setHistory] = useState([])
 
-    const { user: { _id, name, email, role } } = isAuthenticated()
+    const { user: { _id, name, email, role }, token } = isAuthenticated()
+
+    const init = (userId, token) => {
+        getPurchaseHistory(userId, token).then(data => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                setHistory(data)
+            }
+        })
+    }
+
+    useEffect(() => {
+        init(_id, token)
+    }, [])
 
     const userLinks = () => {
         return (
@@ -36,12 +53,26 @@ const Dashboard = () => {
         )
     }
 
-    const purchaseHistory = () => {
+    const purchaseHistory = (history) => {
         return (
             <div className="card mb-5">
                 <h3 className="card-header">Purchase history</h3>
                 <ul className="list-group">
-                    <li className="list-group-item">history</li>
+                    <li className="list-group-item">
+                        {history.map((h, i) => {
+                            return (
+                                h.products.map((p, i) => {
+                                    return (
+                                        <div key={i}>
+                                            <h6>Product name : {p.name}</h6>
+                                            <h6>Product price : {p.price}</h6>
+                                            <h6>Purchase date : {moment(p.createdAt).fromNow()}</h6>
+                                        </div>
+                                    )
+                                })
+                            )
+                        })}
+                    </li>
                 </ul>
             </div>
         )
@@ -55,7 +86,7 @@ const Dashboard = () => {
                 </div>
                 <div className="col-9">
                     {userInfo()}
-                    {purchaseHistory()}
+                    {purchaseHistory(history)}
                 </div>
             </div>
         </Layout>
